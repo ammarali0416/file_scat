c6466a28f317c853812ee88a507b2649eff7e47e
Ammar Syed Ali <ammarali0416@gmail.com>
2025-12-28 14:19:08 -0500

    feat: added cleanup functionality to remove logged files and updated logging mechanism fo performance

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use directories::ProjectDirs;
 use std::path::{Path, PathBuf};
 use std::io::{Write, Result};
-use std::fs::{self, OpenOptions, File};
+use std::fs::{self, OpenOptions, File, read_to_string, remove_file};
 use rand::Rng;
 
 
@@ -16,20 +16,21 @@ struct Args {
     cleanup: bool,
     // Number of files to generate
     #[arg(long, default_value = "10")]
-    count: u32,
+    count: usize,
     // Flag to enable test mode
     #[arg(long, )]
     testing: bool,
 }
 
-fn append_to_log(log_path: &Path, created: &Path) -> Result<()> {
+fn append_to_log(log_path: &Path, file_paths: Vec<PathBuf>) -> Result<()> {
     let mut file = OpenOptions::new()
     .create(true)
     .append(true)
     .open(log_path)?;
 
-    writeln!(file, "{}", created.display())?;
-
+    for path in file_paths.iter() {
+        writeln!(file, "{}", path.display())?;
+    }
 
     file.flush()?;
 
@@ -53,21 +54,6 @@ fn generate_random_filename() -> String {
     return format!("{}.txt", chars);
 }
 
-/// Creates an empty file at the given path, then logs it
-/// Returns Result - can fail if file creation or logging fails
-fn create_file_at_path(file_path: &Path, log_path: &Path) -> Result<()> {
-    // TODO:
-    // 1. Create empty file at file_path using File::create()
-    // 2. Use append_to_log() to log the file_path
-    // 3. Return Ok(()) on success
-    // Hint: Both operations return Result, use ? operator
-    File::create(file_path)?;
-
-    append_to_log(log_path, file_path)?;
-
-    return Ok(());
-}
-
 fn log_path() -> PathBuf {
     let proj = ProjectDirs::from("com", "Ammar_Ali", "file_scat")
     .expect("Could not determine a per-user data directory");
@@ -81,21 +67,59 @@ fn log_path() -> PathBuf {
     return log_path;
 }
 
+/// Reads log file, attempts to delete each file listed
+/// Handles missing files gracefully
+fn cleanup_files(log_path: &Path) -> Result<()> {
+    
+    let log_lines = read_to_string(log_path)?;
+
+    println!("Reading log file at: {}", log_path.display());
+
+    let file_paths = log_lines
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.to_string());
+
+    for file in file_paths {
+        let file_path: PathBuf = PathBuf::from(file);
+        if let Err(e) = remove_file(&file_path) {
+            eprintln!("Could no delete file: {}\n {}", file_path.display(), e)
+        }
+    }
+
+    fs::write(log_path, "")?;
+
+    Ok(())
+}
+
 fn main() {
     let args = Args::parse();
     
     if args.testing {
         println!("Testing mode: Args: {:?}", args);
-        
-        let rand_file_name: String = generate_random_filename();
-        println!("Generated a random filename: {}", rand_file_name);
-
-        let rand_file_path: PathBuf = PathBuf::from("C:/projects/file_scat").join(&rand_file_name);
-        println!("Storing file at path: {}", rand_file_path.display());
 
         let log: PathBuf = log_path();
-        create_file_at_path(&rand_file_path, &log)
-            .expect("Failed to create a test file");
+        
+        let mut file_paths_array: Vec<PathBuf> = Vec::with_capacity(args.count);
+
+        for _i in 1..args.count {
+
+            let rand_file_name: String = generate_random_filename();
+            //println!("Generated a random filename: {}", rand_file_name);
+    
+            let rand_file_path: PathBuf = PathBuf::from("C:/projects/file_scat").join(&rand_file_name);
+            //println!("Storing file at path: {}", rand_file_path.display());
+            
+            File::create(&rand_file_path)
+                .expect("Failed to create file");
+        
+            file_paths_array.push(rand_file_path);
+            
+        }
+
+        println!("{} files created.", file_paths_array.len());
+
+        append_to_log(&log, file_paths_array).expect("Failed to log files");
 
         return;
     }
@@ -105,14 +129,10 @@ fn main() {
     if args.cleanup {
         println!("Mode: cleanup");
         println!("Log file: {}", log.display());
+
+        cleanup_files(&log).expect("Cleanup failed");
+        println!("Cleanup complete");        
     } else {
-        println!("Mode: generate");
-        println!("Count: {}", args.count);
-        println!("Testing: {}", args.testing);
-        println!("Log file: {}", log.display());
-    
-        let fake = std::path::PathBuf::from("some_fake_file.txt");
-        append_to_log(&log, &fake).expect("Failed to append to log");
-        println!("Appended to log: {}", log.display());
+        todo!();
     }
 }
